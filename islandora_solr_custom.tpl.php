e05feec21910e132448ec3c29358b01a1f1b84c5
Danny Joris <danny_joris@yahoo.co.uk>
2011-06-02 13:53:20 -0500

    first commit. backup.

---
+++ islandora_solr_custom.tpl.php
@@ -0,0 +1,14 @@
+<?php
+// $Id $
+/**
+ * @file islandora_solr_custom.tpl.php
+ * Islandora solr search results template
+ *
+ * Variables available:
+ * - $base_url: The base url of the current website. eg: http://example.com .
+ * - $user: The user object.
+ *
+ */
+?>
+<p><?php print t("Your islandora solr search display settings aren't configured yet. Please follow the instructions at the <a href='@admin-page'>Islandora Solr customization page</a>", array('@admin-page' => url('admin/settings/islandora_solr_search/custom') )); ?></p>
+
