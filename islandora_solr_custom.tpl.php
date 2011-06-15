a7e58e45a0120e6b4e0e268072228b4c8993262e
Danny Joris <danny_joris@yahoo.co.uk>
2011-06-15 11:53:35 -0500

    Added the processing for the table display and the initial steps for the display switch

--- islandora_solr_custom.tpl.php
+++ islandora_solr_custom.tpl.php
@@ -8,34 +8,50 @@
  * - $base_url: The base url of the current website. eg: http://example.com .
  * - $user: The user object.
  *
+ * - $style: the style of the display ('div' or 'table')
+ * - $results: the array containing the solr search results
+ * - $table_rendered: if the display style is set to 'table', this will contain the rendered table
+ *
  */
 ?>
-<ul class="islandora_solr_results">
-<?php foreach ($results as $id => $result): ?>
-  <li class="islandora_solr_result">
-  <?php foreach ($result as $field => $values): ?> 
-    <?php 
-      $value = $values['value'];
-      $label = $values['label'];
-      $class = $values['class'];
-      $exclude_label = $values['exclude_label'];
-      $markup = $values['markup'];
-      $zebra = $values['zebra'];
-    ?>
-    <?php if($markup == 1): ?>
-      <div class="solr-field <?php print $class.' '.$zebra ?>">  
-        <?php if($exclude_label == 0): ?>
-          <div class="label">
-            <label><?php print t($label); ?></label>
-          </div>
-        <?php endif; ?>
-        <div class="value"><?php print $value; ?></div>
-      </div>
-    <?php endif; ?>
-    
-  <?php endforeach; ?>
-  </li>
-<?php endforeach; ?>
-</ul>
 
-<?php dsm($variables);
+<?php print $switch_rendered; ?>
+
+<?php if ($style == 'div'): ?>
+
+  <ul class="islandora_solr_results">
+    <?php foreach ($results as $id => $result): ?>
+      <li class="islandora_solr_result">
+        <?php foreach ($result as $field => $values): ?> 
+          <?php 
+            $value = $values['value'];
+            $label = $values['label'];
+            $class = $values['class'];
+            $exclude_label = $values['exclude_label'];
+            $markup = $values['markup'];
+            $zebra = $values['zebra'];
+          ?>
+          <?php if ($markup == 1): ?>
+            <div class="solr-field <?php print $class.' '.$zebra ?>">  
+              <?php if ($exclude_label == 0): ?>
+                <div class="label">
+                  <label><?php print t($label); ?></label>
+                </div>
+              <?php endif; ?>
+              <div class="value"><?php print $value; ?></div>
+            </div>
+          <?php endif; ?>
+    
+        <?php endforeach; ?>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+
+<?php elseif ($style == 'table'): ?>
+
+  <?php print $table_rendered; ?>
+
+<?php endif; ?> 
+
+
+<?php
